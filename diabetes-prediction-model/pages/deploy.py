3ed858dcc60e56940caca9dc7aa1a546acb94032
Madhurjya Dasgupta <dasguptamadhurjya@gmail.com>
2023-03-24 09:53:57 +0530

    Wrote dataset to CSV

--- diabetes-prediction-model/pages/deploy.py
+++ diabetes-prediction-model/pages/deploy.py
@@ -8,48 +8,55 @@ data = pd.read_csv("assets/diabetes.csv")
 headings = data.columns.to_list()
 headings.remove("class")
 
-csv_write = ['No']*16
-
 st.markdown("# Diabetes prediction model")
 checks = st.columns(4)
-data = list()
-if st.checkbox('Age'):
-    data.append(0)
-if st.checkbox('Gender'):
-    data.append(1)
+data = [0]*15
+age = st.slider("Your Age", 1, 70)
+data[0] = age
+
+gender = st.text_area("Enter 1 for male, 0 for female")
+data[1] = gender
 if st.checkbox('Polyuria'):
-    data.append(2)
+    data[2]=1
 if st.checkbox('Polydipsia'):
-    data.append(3)
+    data[3]=1
 if st.checkbox('sudden weight loss'):
-    data.append(4)
+    data[4]=1
 if st.checkbox('weakness'):
-    data.append(5)
+    data[5]=1
 if st.checkbox('Polyphagia'):
-    data.append(6)
+    data[6]=1
 if st.checkbox('Genital thrush'):
-    data.append(7)
+    data[7]=1
 if st.checkbox('visual blurring'):
-    data.append(8)
+    data[8]=1
 if st.checkbox('Itching'):
-    data.append(9)
+    data[9]=1
 if st.checkbox('Irritability'):
-    data.append(10)
+    data[10]=1
 if st.checkbox('delayed healing'):
-    data.append(11)
+    data[11]=1
 if st.checkbox('partial paresis'):
-    data.append(12)
+    data[12]=1
 if st.checkbox('muscle stiffness'):
-    data.append(13)
+    data[13]=1
 if st.checkbox('Alopecia'):
-    data.append(14)
+    data[14]=1
 if st.checkbox('Obesity'):
-    data.append(15)
+    data[15]=1
 
 if st.button('Predict'):
-    st.write(data)
-    # with open("../prediction.csv", "w", newline="") as f:
-    #     writer = csv.writer(f)
-    #     writer.writerow(headings)
-    #     for i in range(len(headings)):
-    #         if 
+    ## Dump the dataset to CSV
+    data[1] = int(data[1])
+    write_data = [age] + [0]*16
+    for i in range(2, len(data)):
+        if data[i] == 1:
+            write_data[i] = 1
+    write_data.pop()
+    st.write(write_data)
+    with open("prediction.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(headings)
+        writer.writerow(write_data)
+
+    diabetes = pickle.load(open('assets/diabetes_model', 'rb'))
