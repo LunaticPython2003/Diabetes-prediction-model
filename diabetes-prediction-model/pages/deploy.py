a6d4dbf364e8b0156eea8aafac8d8612e1298400
Madhurjya Dasgupta <dasguptamadhurjya@gmail.com>
2023-03-24 13:57:27 +0530

    Generated diabetes output

--- diabetes-prediction-model/pages/deploy.py
+++ diabetes-prediction-model/pages/deploy.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pickle
 import pandas as pd
 import csv
+import os
 st.set_page_config(page_title='Health Bridge', initial_sidebar_state='auto')
 
 data = pd.read_csv("assets/diabetes.csv")
@@ -14,8 +15,15 @@ data = [0]*15
 age = st.slider("Your Age", 1, 70)
 data[0] = age
 
-gender = st.text_area("Enter 1 for male, 0 for female")
-data[1] = gender
+option = st.selectbox(
+    "Select your gender",
+    ('Gender','Male', 'Female'))
+
+if option == 'Male':
+    option = 1
+else:
+    option = 0
+
 if st.checkbox('Polyuria'):
     data[2]=1
 if st.checkbox('Polydipsia'):
@@ -46,17 +54,25 @@ if st.checkbox('Obesity'):
     data[15]=1
 
 if st.button('Predict'):
-    ## Dump the dataset to CSV
-    data[1] = int(data[1])
     write_data = [age] + [0]*16
+    data[1] = option
     for i in range(2, len(data)):
         if data[i] == 1:
             write_data[i] = 1
     write_data.pop()
-    st.write(write_data)
     with open("prediction.csv", "w", newline="") as f:
         writer = csv.writer(f)
         writer.writerow(headings)
         writer.writerow(write_data)
+    csv_file = "prediction.csv"
+    diabetes = pd.read_csv(csv_file)
 
-    diabetes = pickle.load(open('assets/diabetes_model', 'rb'))
+    diabetes_model = pickle.load(open('assets/diabetes_model', 'rb'))
+    predictions = diabetes_model.predict(diabetes)
+
+    if predictions[0]:
+        st.markdown("## You are suffering from diabetes")
+    else:
+        st.markdown("## Congratulations, you are not suffering from diabetes!")
+
+    os.remove("prediction.csv")
